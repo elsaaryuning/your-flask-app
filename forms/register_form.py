ff85298dd5f9274953a483b0f56d697c9fc5601f
elsaaryuning <aryuningelsa@gmail.com>
2024-11-16 09:14:21 +0700

    Add files via upload

---
+++ forms/register_form.py
@@ -0,0 +1,12 @@
+# forms/register_form.py
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
